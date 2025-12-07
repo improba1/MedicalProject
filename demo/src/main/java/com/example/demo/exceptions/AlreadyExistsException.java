88092372d14df8d9bcd042e3d8105329b7a0924a
improba1 <narbutviktoria6@gmail.com>
2025-12-07 21:19:13 +0100

    deleted demo from repo

--- demo/src/main/java/com/example/demo/exceptions/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exceptions;
-
-public class AlreadyExistsException extends RuntimeException{
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
