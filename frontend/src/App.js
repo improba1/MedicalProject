fa518115f1f1de2eeedf1463e2ea326ddeb90eb2
improba1 <narbutviktoria6@gmail.com>
2025-12-15 19:07:50 +0100

    added edit profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,8 @@ import CreateReferral from './Pages/CreateReferral/CreateReferral';
 import Ai from './Pages/Ai/Ai';
 import PatientDetails from './Pages/PatientDetails/PatientDetails';
 import AppointmentDetails from './Pages/AppointmentDetails/AppointmentDetails';
+import EditRaport from './Pages/EditRaport/EditRaport';
+import EditProfile from './Pages/EditProfile/EditProfile';
 
 
 import AdminHomePage from './Pages/AdminHomePage/AdminHomePage';
@@ -44,6 +46,8 @@ function App() {
         <Route path="/create-referral" element={<CreateReferral />} />
         <Route path="/ai" element={<Ai />} />
         <Route path="/appointment-details" element={<AppointmentDetails />} />
+        <Route path="/edit-raport" element={<EditRaport />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
         
 
 
