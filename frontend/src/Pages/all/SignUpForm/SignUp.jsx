ae10e3dbae792d56e29ce5e25a9d66efee9a60f0
improba1 <narbutviktoria6@gmail.com>
2025-12-20 18:22:46 +0300

    template for register api

--- frontend/src/Pages/all/SignUpForm/SignUp.jsx
+++ frontend/src/Pages/all/SignUpForm/SignUp.jsx
@@ -3,40 +3,79 @@ import styles from './SignUp.module.css';
 import { Link } from 'react-router-dom';
 import AnimatedPage from '../../../Components/AnimatedPage/AnimatedPage';
 import BackButton from '../../../Components/BackButton/BackButton';
+import { useNavigate } from 'react-router-dom';
+import { authApi } from '../../../Api/authApi';
 
 
 const SignUpForm = () => {
+
+    // 1. Состояние (State) для хранения того, что вводит пользователь
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastNAme] = useState('');
+    const [age, setAge] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault(); // Чтобы страница не перезагружалась
+        setError(''); // Очищаем старые ошибки
+
+        try {
+            // axios.post('куда', { данные })
+            const data = await authApi.register(firstName, lastName, age, email, phone, login, password);
+            //localStorage.setItem('token', data.token);
+            //localStorage.setItem('role', data.role);
+
+            if (data.role === 'DOCTOR') {
+                 navigate('/login'); 
+            } else {
+                setError('Wrong input');
+            }
+
+        } catch (err) {
+            console.error("Login error:", err);
+            setError('Wrong input');
+        }
+    };
+
     return(
             <AnimatedPage>
                 <div className={styles.pageContainer}>
                     <div className={styles.wrapper}>
-                        <form action=""> 
+                        <form action="" onSubmit={handleRegister}> 
                                 <div className={styles.header}>
                                     <BackButton className={styles.backBtn} />
                                     <h1 className={styles.title}>Sign Up</h1>
                                 </div>
 
                                     <div className={styles.inputBox}>
-                                        <input type="text" placeholder="First name"></input>
+                                        <input required type="text" placeholder="First name" value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
                                     </div>
                                     <div className={styles.inputBox}>
-                                        <input type="text" placeholder="Last name"></input>
+                                        <input required type="text" placeholder="Last name" value={lastName} onChange={(e) => setLastNAme(e.target.value)}></input>
                                     </div>
                                     <div className={styles.inputBox}>
-                                        <input type="text" placeholder="Age"></input>
+                                        <input required type="text" placeholder="Age" value={age} onChange={(e) => setAge(e.target.value)}></input>
                                     </div>
                                     <div className={styles.inputBox}>
-                                        <input type="text" placeholder="Email"></input>
+                                        <input required type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
                                     </div>
                                     <div className={styles.inputBox}>
-                                        <input type="text" placeholder="Phone"></input>
+                                        <input required type="text" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
                                     </div>
                                     <div className={styles.inputBox}>
-                                        <input type="text" placeholder="Login"></input>
+                                        <input required type="text" placeholder="Login" value={login} onChange={(e) => setLogin(e.target.value)}></input>
                                     </div>
                                     <div className={styles.inputBox}>
-                                        <input type="text" placeholder="Password"></input>
+                                        <input required type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                                     </div>
+
+                                    {error && <div style={{color: 'red'}}></div>}
 
                                 <button type="submit" className={styles.submitBtn}>Sign Up</button>
 
