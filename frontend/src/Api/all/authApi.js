f7ac43a9b90dd079570aa835062b21fea00890a9
improba1 <narbutviktoria6@gmail.com>
2025-12-21 21:42:10 +0300

    removed src from gitignore, added api for login, sign in, profile

---
+++ frontend/src/Api/all/authApi.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const BASE_URL = '/api/v1/auth'; 
+
+export const authApi = {
+    login: async (login, password) => {
+        const response = await axios.post(`${BASE_URL}/authenticate`, {
+            login, 
+            password
+        });
+        return response.data; 
+    },
+
+    register: async (userData) => {
+        const response = await axios.post(`${BASE_URL}/register`, userData);
+        return response.data;
+    },
+
+    logout: async () => {
+        const token = localStorage.getItem('access_token');
+        
+        return axios.post(`${BASE_URL}/logout`, {}, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    }
+};
---
+++ frontend/src/Api/doctor/scheduleApi.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const $api = axios.create({
+    baseURL: '/api/v1/doctor/me' // Или твой полный адрес
+});
+
+$api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export const scheduleApi = {
+    addAvailability: async (doctorId, dateTimeString) => {
+        // dateTimeString должен быть формата "2023-12-31T14:30:00"
+        const response = await $api.post('/availability', { 
+            doctorId: doctorId,
+            availableTime: dateTimeString
+        });
+        return response.data;
+    }
+};
