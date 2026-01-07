e7c925fa55a626053d52736431ca96bdcacad522
improba1 <narbutviktoria6@gmail.com>
2026-01-07 17:01:45 +0300

    added link to healthcare text

--- frontend/src/Components/HealthcareText/Healthcare.jsx
+++ frontend/src/Components/HealthcareText/Healthcare.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './Healthcare.module.css';
+import { Link } from 'react-router-dom';
 
 const Healthcare = () => {
+    let link;
+    if (localStorage.role === 'DOCTOR'){
+        link = '/doc-home-page';
+    }else if (localStorage.role === 'PATIENT'){
+        link = '/patient';
+    }else if(localStorage.role === 'ADMIN'){
+        link = '/admin';
+    }
     return (
-        <span className={styles.healthcare}>Healthcare</span>
+        <Link to={link}>
+            <span className={styles.healthcare}>Healthcare</span>
+        </Link>
     );
 };
 
