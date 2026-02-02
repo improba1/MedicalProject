62b148b71ca077712aa1a7c69bf3a9d02f203531
gleb_dumanskiy <dumanskiygleb@gmail.com>
2026-02-02 16:52:38 +0100

    pln to usd

--- backend/src/main/java/com/example/demo/service/payment/PaymentServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/payment/PaymentServiceImpl.java
@@ -81,7 +81,7 @@ public class PaymentServiceImpl implements PaymentService {
                                     .setQuantity(1L)
                                     .setPriceData(
                                             SessionCreateParams.LineItem.PriceData.builder()
-                                                    .setCurrency("pln")
+                                                    .setCurrency("usd")
                                                     .setUnitAmount(amountInCents)
                                                     .setProductData(
                                                             SessionCreateParams.LineItem.PriceData.ProductData.builder()
